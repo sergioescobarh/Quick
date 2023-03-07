573c5f7fa3cee1a742419db0c5232d0b8f5cf455
sergioescobarh <sergio.escobar.henao@gmail.com>
2023-03-06 21:18:31 -0500

    styling

---
+++ quick/src/components/ToDo/ToDo.js
@@ -0,0 +1,11 @@
+import './toDo.css';
+
+function ToDo (){
+    return(
+        <div>
+        Todooo
+        </div>
+    )
+}
+
+export {ToDo};
--- quick/src/components/Today/Today.js
+++ quick/src/components/Today/Today.js
@@ -1,4 +1,9 @@
 import './today.css';
+import '../ToDo/ToDo';
+import { ToDo } from '../ToDo/ToDo';
+
+//settting default values for to dos
+
 
 function Today () {
     return(
@@ -7,6 +12,10 @@ function Today () {
     <h2 >TODAY</h2>
     <h4>Sábado, 25 de Junio de 2022</h4>
     <p>hjkhjjhj</p>
+
+    <div className="todo-container">
+        <ToDo/>
+    </div>
 
     </div>
     )
